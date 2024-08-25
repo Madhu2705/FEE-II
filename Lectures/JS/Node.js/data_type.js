8fd74ab65f7df0aa0259cff6f8a7fdb8c73848e6
Madhu <madhu2056.be23@chitkara.edu.in>
2024-08-25 11:18:40 +0530

    upload

--- Lectures/JS/Node.js/data_type.js
+++ Lectures/JS/Node.js/data_type.js
@@ -1,16 +1,26 @@
-let a=10; //number
-let aa=-5; //number
-let aaa=2.5; //number
-let b="hello"; //string
-let c=false; //boolean
-d=20;//value of d
-let j=null;//object
 
-
+//1.number
+let b = -5;
+let c = 2.5;
 console.log(typeof(a))
-console.log(typeof(aa))
-console.log(typeof(aaa))
 console.log(typeof(b))
 console.log(typeof(c))
-console.log(typeof(d))
+
+// 2. string:
+let name = 'shiv';
+console.log(typeof(name))
+
+// 3. boolean:
+let x = false;
+console.log(typeof(x));
+
+// undefined:
+let y;
+console.log(typeof(y))
+y = 20;
+console.log(y)
+console.log(typeof(y));
+
+// null: type->object
+let j = null;
 console.log(typeof(j))
