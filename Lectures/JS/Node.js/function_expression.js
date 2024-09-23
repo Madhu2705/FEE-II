39082049d2ae6e2e1980d70d6dbdd9d3c79218b8
Madhu <madhu2056.be23@chitkara.edu.in>
2024-09-23 14:54:27 +0530

    .

---
+++ Lectures/JS/Node.js/function_expression.js
@@ -0,0 +1,18 @@
+var num=200;
+function cube(num){
+    return num**3;
+}
+console.log(cube(3));
+
+
+
+
+//function expression as a anonymous function
+//anonymous function is a function without any name.
+//const add=function (a,b){
+  //  return a+b;
+
+//};
+//console.log(add);
+//console.log(sum(5,3));
+//console.log(add(5,3));
