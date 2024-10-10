7c8d02d48a3e53efca15314483abd8f3336b930b
Madhu <madhu2056.be23@chitkara.edu.in>
2024-10-10 10:22:49 +0530

    .

--- Lectures/JS/Node.js/callback_hell.js
+++ Lectures/JS/Node.js/callback_hell.js
@@ -1,39 +1,34 @@
-function DownloadFile(url,download){//download,compress or upload is callback function
-console.log("started Downloading");
-settimeout(()=>{
-let path=url.aplit("/").pop();
-download(path);
-},2000);
+function DownloadFile(url, downloadCallback) { //downloadCallback is the callback function
+    console.log("Started Downloading");
+    setTimeout(() => { // Fixed the spelling
+        let path = url.split("/").pop();
+        downloadCallback(path);
+    }, 2000);
 }
 
-function compressfile(path,compress){
-    console.log("file started compressing");
-    settimeout(()=>{
-        const comp=path.split("."[0]+".zip";
-            compress(comp);
-        
-    },2000);
+function compressfile(path, compressCallback) { //compressCallback is the callback function
+    console.log("File started compressing");
+    setTimeout(() => { // Fixed the spelling
+        const comp = path.split(".")[0] + ".zip";
+        compressCallback(comp);
+    }, 2000);
 }
 
-function uploadfile(path,compress){
-    console.log("file started downloading");
-    settimeout(()=>{
-        const url="https://LocalStorage"+comp;
-           uploadfile(upl);
-        
-    },2000);
+function uploadfile(path, uploadCallback) { //uploadCallback is the callback function
+    console.log("File started uploading");
+    setTimeout(() => { // Fixed the spelling
+        const url = "https://LocalStorage/" + path;
+        uploadCallback(url);
+    }, 2000);
 }
-DownloadFile("https://www.xyz.com/abc.mp3",
-    function(path){
-        console.log('file downloaded successfully ${path');
-        compressfile(path,function(comp){
-            console.log('file compressed successfully ${comp}');
-            uploadfile(comp,function(upl){
-console.log('file upload successfully ${upl}'
-);
-console.log("everything is done");            
-});
+
+DownloadFile("https://www.xyz.com/abc.mp3", function (path) {
+    console.log(`File downloaded successfully: ${path}`);
+    compressfile(path, function (comp) {
+        console.log(`File compressed successfully: ${comp}`);
+        uploadfile(comp, function (upl) {
+            console.log(`File uploaded successfully: ${upl}`);
+            console.log("Everything is done");
         });
     });
-
-
+});
