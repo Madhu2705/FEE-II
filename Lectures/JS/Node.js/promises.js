00bb75062c9a4dd84f3a7b364423bd0001943e8a
Madhu <madhu2056.be23@chitkara.edu.in>
2024-10-14 14:17:36 +0530

    .

--- Lectures/JS/Node.js/promises.js
+++ Lectures/JS/Node.js/promises.js
@@ -1,6 +1,6 @@
-// Promises represents a future events.Technically there are objects.By default Promises are in pending states. 
-//Promise state-pending, fullfilled , reject
-//1.How to create promise: it has 2 callbacks - resolve, reject
+/* Promises represents a future events.Technically there are objects.By default Promises are in pending states. 
+Promise state-pending, fullfilled , reject
+1.How to create promise: it has 2 callbacks - resolve, reject
 let p=new Promise((resolve,reject)=>{
     let like=true;
 setTimeout(() => {
@@ -33,14 +33,37 @@ let p1 = new Promise((resolve,reject)=>{
         resolve("resolve is working after 2 sec");
     }, 2000);
 });
-
-p1.then((value)=>{
-    console.log(value);
-});
-
-
 let p2 = new Promise((resolve,reject)=>{
     setTimeout(() => {
         resolve("promise 2 is working");
     }, 2000);
+});*/
+
+//Ques:
+//create a function with name downloadMovie which has url a argument.Create a promise if url is not mp4-> reject else mp4-> resolve:
+function DownloadMovie(url,download) { //download is the callback function
+    console.log("Started Downloading");
+    setTimeout(() => { 
+        let path = url.split("/").pop();
+        download(path);
+    }, 2000);
+}
+let p=new Promise((resolve,reject)=>{
+    let url=true;
+setTimeout(() => {
+    if(like){
+        resolve("Yeah , she like you");
+    }else{
+        reject("naah!! your ugly");
+    }
+    resolve("this is a resolve");
+}, 2000);
 });
+//console.log(p);
+
+
+
+
+
+
+
